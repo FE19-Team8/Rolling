e297865f957126af110daab7c4c2728844d0d782
hyeok <nohyeog8@gmail.com>
2025-09-29 20:56:46 +0900

    feat:api 필수 입력검증 구현

--- src/api/papers.js
+++ src/api/papers.js
@@ -1,4 +1,5 @@
 import api from "./api";
+import { throwIfMissing } from "../utils/validators";
 
 const END_POINT = "/19-8/recipients/";
 
@@ -6,8 +7,11 @@ export const getPaperList = (options = {}) =>
   api.get(END_POINT, {
     params: options,
   });
-export const createPaper = (data) => api.post(END_POINT, data);
+export const createPaper = (data = throwIfMissing("data")) =>
+  api.post(END_POINT, data);
 
-export const getTargetPaper = (id) => api.get(`${END_POINT}${id}/`);
+export const getTargetPaper = (id = throwIfMissing("id")) =>
+  api.get(`${END_POINT}${id}/`);
 
-export const deletePaper = (id) => api.delete(`${END_POINT}${id}/`);
+export const deletePaper = (id = throwIfMissing(id)) =>
+  api.delete(`${END_POINT}${id}/`);
--- src/api/reactions.js
+++ src/api/reactions.js
@@ -1,7 +1,11 @@
 import api from "./api";
+import { throwIfMissing } from "../utils/validators";
 
-export const reactToPaper = (recipient_id) =>
+export const reactToPaper = (recipient_id = throwIfMissing("recipinet_id")) =>
   api.post(`/19-8/recipients/${recipient_id}/reactions/`);
 
-export const getPaperReactions = (recipient_id, options = {}) =>
+export const getPaperReactions = (
+  recipient_id = throwIfMissing("recipinet_id"),
+  options = {}
+) =>
   api.get(`/19-8/recipients/${recipient_id}/reactions/`, { params: options });
