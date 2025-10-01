e10390cf62d65de2d1d796a61a918a970304418f
Bang Dayeon <134805973+Bangdayeon@users.noreply.github.com>
2025-10-01 21:27:20 +0900

    feat: RollingPaperCard 구현 (#9)
    - 251001/12:02 0명 안내 문구 추가

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,7 +1,41 @@
+import RollingPaperCard from "../ListPage/components/RollingPaperCard/RollingPaperCard";
+
 const MainPage = () => {
   return (
-    <div>
-      <p> this is main page.</p>
+    <div className="flex flex-col gap-3 m-3">
+      <div className="flex gap-3">
+        <RollingPaperCard />
+        <RollingPaperCard name="KIM" messageCount={4} backgroundColor="green" />
+        <RollingPaperCard
+          name="MILK"
+          messageCount={30}
+          backgroundColor="blue"
+        />
+        <RollingPaperCard
+          name="sfjkldjfslfjsdfklsjflkfjklfjds"
+          messageCount={400}
+          backgroundColor="purple"
+        />
+      </div>
+      <div className="flex gap-3">
+        <RollingPaperCard messageCount={99999999999} />
+        <RollingPaperCard
+          name="KIM"
+          profiles={[]}
+          messageCount={0}
+          backgroundColor="green"
+        />
+        <RollingPaperCard
+          name="MILK"
+          messageCount={30000000000000000000}
+          backgroundColor="blue"
+        />
+        <RollingPaperCard
+          name="sfjkldjfslfjsdfklsjflkfjklfjds"
+          messageCount={400}
+          backgroundColor="purple"
+        />
+      </div>
     </div>
   );
 };
