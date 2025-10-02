8b67fc9f09afb2febcfeec75ba11cd97cd2127ac
지니 <117137463+yujinimda@users.noreply.github.com>
2025-10-02 16:41:39 +0900

    refator: 공통 Input 컴포넌트 리팩토링 (#50)
    * chore: 공통 Input 컴포넌트 리팩토링 (#50)
    
    * refactor: 공통 Input 컴포넌트 error/disabled를 variant에서 prop으로 분리 (#50)

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,45 +1,30 @@
+import clsx from "clsx";
+
+const BASE_CLASSES =
+  "w-full h-[50px] rounded-lg border outline-none px-4 py-3  transition-colors text-base placeholder-gray-500 text-gray-900 ";
+
+const STATE_CLASSES = {
+  default: "border-gray-300 hover:border-gray-500 focus:border-gray-700",
+  error: "border-error hover:border-error focus:border-error",
+  disabled: "border-gray-300 bg-gray-100 text-gray-400 cursor-not-allowed",
+};
 const Input = ({
   placeholder,
   value,
-  onChange,
-  disabled = false,
+  type = "text",
   error = false,
   errorMessage,
-  type = "text",
+  disabled = false,
+  className,
+  ...props
 }) => {
-  const baseClasses = [
-    "w-full",
-    "h-[50px]",
-    "pt-3",
-    "pr-4",
-    "pb-3",
-    "pl-4",
-    "border",
-    "rounded-lg",
-    "outline-none",
-    "transition-colors",
-    "text-base",
-  ];
-
-  if (error) {
-    baseClasses.push("border-red-500", "bg-red-50");
-  } else if (disabled) {
-    baseClasses.push(
-      "border-gray-300",
-      "bg-gray-100",
-      "text-gray-400",
-      "cursor-not-allowed"
-    );
-  } else {
-    baseClasses.push(
-      "border-gray-300",
-      "bg-white",
-      "hover:border-gray-500",
-      "focus:border-gray-700",
-      "focus:ring-1",
-      "focus:ring-gray-700"
-    );
-  }
+  const inputClasses = clsx(
+    BASE_CLASSES,
+    !error && !disabled && STATE_CLASSES.default,
+    error && STATE_CLASSES.error,
+    disabled && STATE_CLASSES.disabled,
+    className
+  );
 
   return (
     <div className="w-full">
@@ -47,12 +32,13 @@ const Input = ({
         type={type}
         placeholder={placeholder}
         value={value}
-        onChange={onChange}
         disabled={disabled}
-        className={baseClasses.join(" ")}
+        aria-invalid={error}
+        className={inputClasses}
+        {...props}
       />
       {error && errorMessage && (
-        <p className="mt-1 text-error px-1 text-sm">{errorMessage}</p>
+        <p className="mt-2 text-error px-1 text-sm">{errorMessage}</p>
       )}
     </div>
   );
