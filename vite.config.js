962248c36d31b1957f5fcefe018944d665b452cd
지니 <117137463+yujinimda@users.noreply.github.com>
2025-10-05 00:15:57 +0900

    chore: 경로 별칭 설정 (#54)

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
+import { fileURLToPath, URL } from 'url';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,4 +13,9 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 });
