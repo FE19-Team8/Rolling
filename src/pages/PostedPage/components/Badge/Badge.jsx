08419315dda50c8dadbf47c89b0574a26df55824
Bangdayeon <dybang00@gmail.com>
2025-10-06 01:50:15 +0900

    feat: post/{id} 페이지 Badge 컴포넌트 구현 (#100)

---
+++ src/pages/PostedPage/components/Badge/Badge.jsx
@@ -0,0 +1,19 @@
+import clsx from 'clsx';
+
+const Badge = ({ relationship }) => {
+  const baseStyle = 'rounded text-sm px-2';
+  const styles = {
+    지인: 'text-beige500 bg-beige100',
+    가족: 'text-green500 bg-green100',
+    친구: 'text-blue500 bg-blue100',
+    동료: 'text-purple600 bg-purple100',
+  };
+  const classes = clsx(
+    baseStyle,
+    styles[relationship] || 'bg-gray200 text-gray800'
+  );
+
+  return <span className={classes}>{relationship}</span>;
+};
+
+export default Badge;
