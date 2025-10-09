e2166a3419861193db661189db7ddba597ca30bd
Nick <nohyeog8@gmail.com>
2025-10-10 01:58:38 +0900

    feat:헤더 제작용 확장 설치

--- src/pages/MessagePage/ui/RichTextEditor.jsx
+++ src/pages/MessagePage/ui/RichTextEditor.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-
+import { useEditor, EditorContent } from '@tiptap/react';
 function RichTextEditor() {
+  const Editor = useEditor();
   return (
     <div>
       <p></p>
