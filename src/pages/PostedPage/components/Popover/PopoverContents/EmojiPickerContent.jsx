76afb20bb381e1aa203698732c34e08a8989efcc
Bangdayeon <dybang00@gmail.com>
2025-10-11 13:57:09 +0900

    feat: EmojiPicker 컴포넌트 추가

---
+++ src/pages/PostedPage/components/Popover/PopoverContents/EmojiPickerContent.jsx
@@ -0,0 +1,10 @@
+import EmojiPicker from 'emoji-picker-react';
+import { PopoverContent } from './PopoverContent';
+
+export const EmojiPickerContent = ({ onEmojiClick }) => {
+  return (
+    <PopoverContent>
+      <EmojiPicker onEmojiClick={onEmojiClick} />
+    </PopoverContent>
+  );
+};
--- src/pages/PostedPage/components/Popover/index.js
+++ src/pages/PostedPage/components/Popover/index.js
@@ -2,3 +2,4 @@ export { Popover } from './Popover';
 export { PopoverTrigger } from './PopoverTrigger';
 export { ShareContent } from './PopoverContents/ShareContent';
 export { EmojiContent } from './PopoverContents/EmojiContent';
+export { EmojiPickerContent } from './PopoverContents/EmojiPickerContent';
