dd4c37b906920b02a2b6acbade79a6f0e1035ce2
Bangdayeon <dybang00@gmail.com>
2025-10-12 00:47:41 +0900

    refactor: ListPage 리팩토링 (#123)

--- src/pages/ListPage/ListPage.jsx
+++ src/pages/ListPage/ListPage.jsx
@@ -1,45 +1,58 @@
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useRecipients } from './useRecipients';
-
 import Button from '../../components/Button/Button';
 import CardList from './components/CardList/CardList';
-
 const ListPage = () => {
-  // 페이지 이동
-  // const navigate = useNavigate();
-
+  const navigate = useNavigate();
   const { recipients, loading, error } = useRecipients();
-  const popularSorted = [...(recipients.results || [])].sort(
-    (a, b) => b.messageCount - a.messageCount,
-  );
-  const recentSorted = [...(recipients.results || [])].sort(
-    (a, b) => new Date(b.createdAt) - new Date(a.createdAt),
-  );
 
-  if (loading) return <div>로딩중...</div>;
-  if (error) return <div>{error}</div>;
-  if (recipients.length === 0) return <div>no data...</div>;
+  const isEmpty =
+    !recipients.popular?.results?.length && !recipients.recent?.results?.length;
 
-  const sectionStyle = 'flex flex-col gap-[16px] m-[50px]';
-  const titleStyle = 'font-bold text-[24px] leading-[36px] tracking-[-0.01em]';
+  // STYLES
+  const sectionStyle = 'flex flex-col items-center gap-4 m-13 w-fit';
+  const titleStyle = 'font-bold text-2xl leading-9 tracking-[-0.01em] w-full';
+  const ListStyle = 'relative w-full';
 
-  return (
-    <div className="flex flex-col items-center mb-[218px]">
-      <p className="border bg-amber-200 w-full h-[64px]">헤더 자리</p>
-      <div className="mt-[50px] mb-[64px]">
-        <section className={sectionStyle}>
-          <h2 className={titleStyle}>인기 롤링 페이퍼🔥</h2>
-          <CardList cards={popularSorted} />
-        </section>
-        <section className={sectionStyle}>
-          <h2 className={titleStyle}>최근에 만든 롤링 페이퍼⭐</h2>
-          <CardList cards={recentSorted} />
-        </section>
+  if (error) return <div>에러가 발생했습니다.</div>;
+  if (loading) return <div>불러오는 중...</div>;
+  if (isEmpty)
+    return (
+      <div>
+        <h1 className={titleStyle}>아직 등록된 롤링페이퍼가 없어요</h1>
+        <div className="flex justify-center items-center">
+          <Button size="medium" onClick={() => navigate('/post')}>
+            내 공간 만들어보기
+          </Button>
+        </div>
       </div>
-      {/* onClick={() => navigate('/PostedPage')} */}
-      <Button size="medium">나도 만들어보기</Button>
-    </div>
-  );
+    );
+  if (!recipients?.results?.length)
+    return (
+      <div>
+        <div className="flex flex-col lg:items-center mb-[218px]">
+          <div className="mt-[50px] mb-[64px]">
+            <section className={sectionStyle}>
+              <h2 className={titleStyle}>인기 롤링 페이퍼🔥</h2>
+              <div className={ListStyle}>
+                <CardList cards={recipients.popular.results} />
+              </div>
+            </section>
+            <section className={sectionStyle}>
+              <h2 className={titleStyle}>최근에 만든 롤링 페이퍼⭐</h2>
+              <div className={ListStyle}>
+                <CardList cards={recipients.recent.results} />
+              </div>
+            </section>
+          </div>
+          {/* onClick={() => navigate('/PostedPage')} */}
+          <div className="flex justify-center items-center">
+            <Button size="medium" onClick={() => navigate('/post')}>
+              나도 만들어보기
+            </Button>
+          </div>
+        </div>
+      </div>
+    );
 };
-
 export default ListPage;
--- src/pages/ListPage/useRecipients.js
+++ src/pages/ListPage/useRecipients.js
@@ -2,15 +2,18 @@ import api from '@/api/api';
 import { useState, useEffect } from 'react';
 
 export const useRecipients = () => {
-  const [recipients, setRecipients] = useState([]);
+  const [recipients, setRecipients] = useState({ popular: [], recent: [] });
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchRecipients = async () => {
       try {
-        const res = await api.get('/19-8/recipients/');
-        setRecipients(res);
+        const [popularRes, recentRes] = await Promise.all([
+          api.get('/19-8/recipients/?sort=like'),
+          api.get('/19-8/recipients/'),
+        ]);
+        setRecipients({ popular: popularRes, recent: recentRes });
       } catch (error) {
         setError(error);
         console.log('data loading error');
