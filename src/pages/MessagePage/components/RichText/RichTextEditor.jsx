0c70249ab0942b864350f980047e06e37ed4c761
Nick <nohyeog8@gmail.com>
2025-10-12 09:10:34 +0900

    feat: fowardRef 추가

--- src/pages/MessagePage/components/RichText/RichTextEditor.jsx
+++ src/pages/MessagePage/components/RichText/RichTextEditor.jsx
@@ -1,15 +1,13 @@
-import React from 'react';
+import { forwardRef, useImperativeHandle } from 'react';
 import { useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import TextAlign from '@tiptap/extension-text-align';
 import Color from '@tiptap/extension-color';
 import { TextStyle } from '@tiptap/extension-text-style';
-import OrderedList from '@tiptap/extension-ordered-list';
-import BulletList from '@tiptap/extension-bullet-list';
 import Editor from './Editor';
 import Toolbar from './Toolbar/Toolbar';
 
-function RichTextEditor() {
+const RichTextEditor = forwardRef((__, ref) => {
   const editor = useEditor({
     extensions: [
       StarterKit,
@@ -18,12 +16,17 @@ function RichTextEditor() {
       TextAlign.configure({ types: ['heading', 'paragraph', 'listItem'] }),
     ],
   });
+
+  useImperativeHandle(ref, () => ({
+    getValue: () => (editor ? editor.getHTML() : ''),
+  }));
+
   return (
     <div className="w-[720px] h-[260px] border border-gray-300 rounded-[16px] overflow-hidden">
       <Toolbar editor={editor} />
       <Editor editor={editor} />
     </div>
   );
-}
+});
 
 export default RichTextEditor;
