36d2198aa91acdd1d8a74cf9a5fd36fed500d6a1
Bangdayeon <dybang00@gmail.com>
2025-10-13 16:36:51 +0900

    feat: pate{id} Modal 컴포넌트 구현 (#75)

--- src/pages/PostedPage/components/MessageCard/MessageCard.jsx
+++ src/pages/PostedPage/components/MessageCard/MessageCard.jsx
@@ -1,62 +1,82 @@
+import { useState } from 'react';
+
 import Badge from '../Badge/Badge';
 import Divider from '@/components/Divider/Divider';
 import Profile from '@/components/Profile/Profile';
 
-const EX = {
-  sender: '김동훈',
-  profileImageURL: '/images/default_profile.png',
-  relationship: '가족',
-  content:
-    '코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 하세요! 그리고 저는 방금 테마를 바꿨어요 아까는 줄바꿈이 안되더니 왜 갑자기 될까요? 이유를 알수가 없네요 둘리~ 둘리~ 요리보고~ 저리봐도 알 수 없는 레이레이~~',
-  font: 'Pretendard',
-  createdAt: '2023-11-01T08:05:25.399056Z',
-};
+import ModalWrapper from '../Modal/ModalWrapper';
+import MessageContent from '../Modal/MessageContent';
 
 const MessageCard = ({
-  sender = EX.sender,
-  profileImageURL = EX.profileImageURL,
-  relationship = EX.relationship,
-  content = EX.content,
-  font = EX.font,
-  createdAt = EX.createdAt,
+  sender,
+  profileImageURL,
+  relationship,
+  content,
+  font,
+  createdAt,
 }) => {
+  const [isOpen, setIsOpen] = useState(false);
   const date = createdAt.slice(0, 10).replace(/-/g, '.');
 
+  const onClose = () => {
+    setIsOpen(false);
+  };
+
+  const baseStyle = `
+    flex flex-col justify-between p-6 ph-[28px] h-[280px] rounded-[16px] shadow-[0_2px_12px_0_#00000014]
+    hover:cursor-pointer hover:shadow-lg
+  `;
+
   return (
-    <div className="flex flex-col justify-between p-[24px] ph-[28px] h-[280px] rounded-[16px] shadow-[0_2px_12px_0_#00000014]">
-      {/* SENDER */}
-      <div>
-        {/* HEAD */}
-        <div className="flex gap-[14px] mb-[15px]">
-          <Profile src={profileImageURL} bordered />
-          <div>
-            <span className="flex gap-[6px]">
-              <span className="text-[20px] leading-[24px]">From. </span>
-              <span className="text-[20px] leading-[24px] font-bold">
-                {sender}
+    <>
+      <div className={baseStyle} onClick={() => setIsOpen(true)}>
+        {/* SENDER */}
+        <div>
+          {/* HEAD */}
+          <div className="flex gap-[14px] mb-[15px]">
+            <Profile src={profileImageURL} bordered />
+            <div>
+              <span className="flex gap-[6px]">
+                <span className="text-[20px] leading-[24px]">From. </span>
+                <span className="text-[20px] leading-[24px] font-bold">
+                  {sender}
+                </span>
               </span>
+              <Badge relationship={relationship} />
+            </div>
+          </div>
+          <Divider />
+          {/* CONTENT */}
+          <div className="py-[16px]">
+            <span
+              className="text-gray600 text-[18px] leading-[28px] tracking-[-0.01em] line-clamp-4"
+              style={{ fontFamily: font }}
+            >
+              {content}
             </span>
-            <Badge relationship={relationship} />
           </div>
         </div>
-        <Divider />
-        {/* CONTENT */}
-        <div className="py-[16px]">
-          <span
-            className="text-gray600 text-[18px] leading-[28px] tracking-[-0.01em] line-clamp-4"
-            style={{ fontFamily: font }}
-          >
-            {content}
+        {/* DATE */}
+        <div>
+          <span className="text-gray400 text-[12px] tracking-[-0.005em]">
+            {date}
           </span>
         </div>
       </div>
-      {/* DATE */}
-      <div>
-        <span className="text-gray400 text-[12px] tracking-[-0.005em]">
-          {date}
-        </span>
-      </div>
-    </div>
+      {isOpen && (
+        <ModalWrapper onClose={() => setIsOpen(false)}>
+          <MessageContent
+            sender={sender}
+            profileImageURL={profileImageURL}
+            relationship={relationship}
+            content={content}
+            font={font}
+            createdAt={createdAt}
+            onClose={onClose}
+          />
+        </ModalWrapper>
+      )}
+    </>
   );
 };
 
