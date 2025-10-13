91c9dac04a043f3c0712662d96d08d8300730023
hyeok <nohyeog8@gmail.com>
2025-10-13 17:49:23 +0900

    feat: 머지 충돌 해결 (#134)

--- src/pages/PostedPage/PostedPage.jsx
+++ src/pages/PostedPage/PostedPage.jsx
@@ -7,35 +7,19 @@ import MessageCardList from './components/MessageCardList/MessageCardList';
 const PostedPage = () => {
   // const { id } = useParams();
   return (
-<<<<<<< HEAD
     <Layout>
-      <Background
-        color={'beige'}
-        imageURL={'https://picsum.photos/id/1058/3840/2160'}
-      />
-      <div className="min-h-screen flex flex-col">
+      <Background color={'beige'} imageURL={'https://picsum.photos/id/1058/3840/2160'} />
+      <div className="flex min-h-screen flex-col">
         {/* SubHeader 영역 */}
-        <div className="sticky top-[64px] z-40 w-full ">
+        <div className="sticky top-[64px] z-40 w-full">
           <SubHeader />
         </div>
         {/* 카드 리스트 영역 */}
-        <div className="w-full max-w-[1200px] mx-auto xl:mt-28 xl:p-0 sm:mt-8  sm:p-6 mt-8 p-5">
+        <div className="mx-auto mt-8 w-full max-w-[1200px] p-5 sm:mt-8 sm:p-6 xl:mt-28 xl:p-0">
           <MessageCardList />
         </div>
       </div>
     </Layout>
-=======
-    <div className="flex min-h-screen flex-col">
-      {/* SubHeader 영역 */}
-      <div className="sticky top-[64px] z-40 w-full">
-        <SubHeader />
-      </div>
-      {/* 카드 리스트 영역 */}
-      <div className="mx-auto mt-8 w-full max-w-[1200px] p-5 sm:mt-8 sm:p-6 xl:mt-28 xl:p-0">
-        <MessageCardList />
-      </div>
-    </div>
->>>>>>> 4f8c98a (chore: 머지 컨플릭트 해결 prettier (#134))
   );
 };
 
