6c58ce88a1320b9ea71c3347eee2049de850ec24
지니 <117137463+yujinimda@users.noreply.github.com>
2025-10-13 14:22:38 +0900

    feat: infiniteScroll 구현 (#70)
    * feat: infiniteScroll 구현 (#70)
    
    * feat: infiniteScroll 훅 구현 (#70)

--- src/pages/PostedPage/components/MessageCardList/MessageCardList.jsx
+++ src/pages/PostedPage/components/MessageCardList/MessageCardList.jsx
@@ -2,8 +2,17 @@ import { Link } from 'react-router-dom';
 import Button from '@/components/Button/Button';
 import MessageCard from '@/pages/PostedPage/components/MessageCard/MessageCard';
 import { MOCK_MESSAGES } from '@/pages/PostedPage/components/MessageCardList/messages';
+import Skeleton from '@/components/Skeleton/Skeleton';
+import useInfiniteScrollMessages from '@/pages/PostedPage/hooks/useInfiniteScroll';
 
 const MessageCardList = () => {
+  const { displayedMessages, observerRef, isLoading } =
+    useInfiniteScrollMessages({
+      data: MOCK_MESSAGES,
+      initialCount: 5,
+      nextCount: 3,
+    });
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6">
       {/* 첫 번째 메세지 카드 고정*/}
@@ -15,7 +24,7 @@ const MessageCardList = () => {
         <Button shape="circle" iconName="plus" variant="gray" />
       </Link>
       {/* 카드 리스트 */}
-      {MOCK_MESSAGES.map((msg) => (
+      {displayedMessages.map((msg) => (
         <MessageCard
           key={msg.id}
           sender={msg.sender}
@@ -25,6 +34,24 @@ const MessageCardList = () => {
           createdAt={msg.createdAt}
         />
       ))}
+
+      {/* 감시용 div (맨 아래) */}
+      <div ref={observerRef} className="h-[1px] col-span-full bg-transparent" />
+
+      {/* 로딩  스켈레톤 */}
+      {isLoading && (
+        <>
+          {Array.from({ length: 3 }).map((_z, i) => (
+            <Skeleton
+              key={i}
+              variant="base"
+              shape="rect"
+              width="100%"
+              height="280px"
+            />
+          ))}
+        </>
+      )}
     </div>
   );
 };
---
+++ src/pages/PostedPage/hooks/useInfiniteScroll.js
@@ -0,0 +1,49 @@
+import { useEffect, useRef, useState } from 'react';
+
+export default function useInfiniteScrollMessages({
+  data,
+  initialCount = 5,
+  nextCount = 3,
+}) {
+  const [displayedMessages, setDisplayedMessages] = useState(
+    data.slice(0, initialCount),
+  );
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const observerRef = useRef(null);
+
+  useEffect(() => {
+    const callback = (entries) => {
+      const entry = entries[0];
+      if (!entry.isIntersecting || isLoading) return;
+      if (displayedMessages.length >= data.length) return;
+
+      setIsLoading(true);
+      setTimeout(() => {
+        const nextPage = page + 1;
+        const nextCountTotal = initialCount + (nextPage - 1) * nextCount;
+        const newItems = data.slice(0, nextCountTotal);
+
+        setDisplayedMessages(newItems);
+        setPage(nextPage);
+        setIsLoading(false);
+      }, 500);
+    };
+
+    const observer = new IntersectionObserver(callback, {
+      root: null,
+      rootMargin: '0px 0px 200px 0px',
+      threshold: 0,
+    });
+
+    const target = observerRef.current;
+    if (target) observer.observe(target);
+
+    return () => {
+      if (target) observer.unobserve(target);
+      observer.disconnect();
+    };
+  }, [displayedMessages.length, isLoading, data, page]);
+
+  return { displayedMessages, observerRef, isLoading };
+}
