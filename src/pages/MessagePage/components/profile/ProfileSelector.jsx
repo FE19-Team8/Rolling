49d4b57690c7a4c9bd00ccc9fd1e503e63072723
hyeok <nohyeog8@gmail.com>
2025-10-13 17:11:16 +0900

    feat: fowardRef dispatch 수정 (#134)

--- src/pages/MessagePage/components/profile/ProfileSelector.jsx
+++ src/pages/MessagePage/components/profile/ProfileSelector.jsx
@@ -1,10 +1,10 @@
 import { getProfileImages } from '@/api/images';
-import { useState, useEffect, forwardRef, useImperativeHandle } from 'react';
+import { useState, useEffect } from 'react';
 import ProfileList from './ProfileList';
 import Profile from '@/components/Profile/Profile';
 import Skeleton from '@/components/Skeleton/Skeleton';
 
-const ProfileSelector = forwardRef((__, ref) => {
+function ProfileSelector() {
   const [selected, setSelected] = useState(null);
   const [profileImgs, setProfileImgs] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
@@ -26,14 +26,12 @@ const ProfileSelector = forwardRef((__, ref) => {
     fetchProfiles();
   }, []);
 
-  useImperativeHandle(ref, () => ({ getValue: () => selected }));
-
   if (isLoading) {
     return <ProfileSelectorSkeleton />;
   }
 
   return (
-    <fieldset className="flex items-center gap-8 py-2 w-[717px] h-[142px]">
+    <fieldset className="flex h-[142px] w-[717px] items-center gap-8 py-2">
       <Profile src={selected} size="large" />
       <div className="flex flex-col gap-4">
         <legend className="text-gray-500">프로필 이미지를 선택해주세요!</legend>
@@ -41,18 +39,18 @@ const ProfileSelector = forwardRef((__, ref) => {
       </div>
     </fieldset>
   );
-});
+}
 
 const ProfileSelectorSkeleton = () => {
   const PROFILE_IMG_LENGTH = 9;
 
   return (
-    <div className="flex items-center gap-8 py-2 w-[717px] h-[142px]">
+    <div className="flex h-[142px] w-[717px] items-center gap-8 py-2">
       <Skeleton shape="circle" width="80px" height="80px" />
 
       <div className="flex flex-col gap-4">
         <Skeleton shape="text" width="200px" height="24px" />
-        <div className="flex w-full gap-2 h-[56px]">
+        <div className="flex h-[56px] w-full gap-2">
           {[...Array(PROFILE_IMG_LENGTH)].map((__, index) => (
             <Skeleton key={index} shape="circle" width="56px" height="56px" />
           ))}
